f4b45d4c549cf95a404ce8c747794f94188f70d2
Tim Dugan <tadugan@me.com>
2021-11-16 15:45:59 -0600

    Fixed bug in tailwind.config.js. Tooltips work as intended

--- src/SideBarIcon.js
+++ src/SideBarIcon.js
@@ -1,8 +1,10 @@
 function SideBarIcon({ icon, text = 'tooltip' }) {
     return (
-        <div className='sidebar-icon'>
+        <div className='sidebar-icon group'>
             {icon}
-            <span className='sidebar-tooltip'>{text}</span>
+            <span className='sidebar-tooltip group-hover:scale-100'>
+                {text}
+            </span>
         </div>
     );
 }
